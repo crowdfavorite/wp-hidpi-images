2760c84781400af3f4fc21c1a47688fd91f3f014
Evan Anderson <ejdanderson@gmail.com>
2013-07-22 13:50:27 -0600

    Initial commit

---
+++ wp-hidpi-images.php
@@ -0,0 +1,9 @@
+<?php
+/*
+Plugin Name: WP HiDPI Images
+Plugin URI: http://crowdfavorite.com/wordpress/plugins/ 
+Description: Create and insert high resolution images to support high DPI displays.
+Version: 0.1
+Author: Crowd Favorite
+Author URI: http://crowdfavorite.com
+*/
